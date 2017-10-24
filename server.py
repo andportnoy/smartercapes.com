a86adafdb43f668fef590be148323b2f87400984
Andrey Portnoy <aportnoj@gmail.com>
2017-10-23 18:30:50 -0700

    initial commit

---
+++ server.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template
+import json
+app = Flask(__name__)
+
+def load_json():
+    with open('ranking.json') as f:
+        ranking = json.load(f)
+    with open('time.json') as f:
+        time = json.load(f)
+    with open('grade.json') as f:
+        grade = json.load(f)
+    return ranking, time, grade
+
+ranking, time, grade = load_json()
+print('Loaded json')
+
+@app.route("/")
+def hello():
+    return render_template('home.html')
+
+@app.route("/<dept>/<course>")
+def welcome(dept, course):
+    code = (dept + ' ' + course).upper()
+    try:
+        rank = ranking[code]
+        hours = time[code]['time']
+        time_color = time[code]['color']
+        time_statement = time[code]['statement']
+        
+        expected_grade = grade[code]['expected_letter_grade']
+        grade_color = grade[code]['color']
+        grade_statement = grade[code]['statement']
+    except KeyError:
+        return render_template('nodata.html', code=code)
+    
+    return render_template("report.html", code=code, rank=rank, 
+        hours=hours, time_color=time_color, time_statement=time_statement, 
+        expected_grade=expected_grade, grade_color=grade_color, grade_statement=grade_statement)
+    
+app.run(host='0.0.0.0', port=80)
