e467268b17ac2ae131c89aa4f8131fe524c6ae16
Andrey Portnoy <aportnoj@gmail.com>
2017-10-28 18:59:51 -0700

    fix js

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,7 @@
 function buildSelect2(value1) {
     var select2 = document.getElementById("select2");
-        console.log("value is");
-        console.log(value1);
+    console.log("value is");
+    console.log(value1);
     if (value1 == "default") {
         select2.disabled = true;
     } else {
